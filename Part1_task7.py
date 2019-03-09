8b292043279a41f820f98b89f8c7af1f0ac33a63
Alex <alex_jel@list.ru>
2019-03-09 23:57:43 +0400

    Finished 1 task in Part1_task7

--- Part1_task7.py
+++ Part1_task7.py
@@ -1,6 +1,31 @@
 def task_1():
-    arr = *map(int, input().split())
-    print(arr)
+    arr = list(map(int, input().split()))
     
+    print("part 1:", end = " ")
+    ans = True
+    prev = 0
+    part_2_ans = ()
+    for i in arr:
+        if i % 2 == 0:
+            print(i, end = " ")
+            
+        if ans and prev * i > 0:
+            part_2_ans = (prev, i)
+            ans = False
+        prev = i
+    
+    print("\npart 2:", *part_2_ans)
+    
+    print("part 3:\nmin, max:", min(arr), max(arr))
+    print("indexes:", arr.index(min(arr)), arr.index(max(arr)))
+    
+    arr.sort()
+    ans_4 = 0
+    prev = "0"
+    for i in arr:
+        if i != prev:
+            ans_4 += 1
+        prev = i
+    print("part 4:", ans_4)
     
 task_1()
