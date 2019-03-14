2f6aebc790e46c8961f7d9c7d7201254760a40b0
Alex <alex_jel@list.ru>
2019-03-14 14:51:31 +0400

    Finished part1_task7.

--- Part1_task7.py
+++ Part1_task7.py
@@ -28,4 +28,88 @@ def task_1():
         prev = i
     print("part 4:", ans_4)
     
-task_1()
+    
+def task_2():
+    arr = list(map(int, input().split()))
+    
+    arr_x = arr_y = diag_l = diag_r = []
+    ans = True
+    for i in range(0, len(arr), 2):
+        x = arr[i]
+        y = arr[i + 1]
+        d_l = x / y
+        d_r = y / x
+        
+        check = arr_x.count(x) + arr_y.count(y) + diag_l.count(d_l) + diag_r.count(d_r)
+        if (check > 0):
+            ans = False
+        
+        arr_x.append(x)
+        arr_y.append(y)
+        diag_l.append(d_l)
+        diag_r.append(d_r)
+    if ans:
+        print("NO")
+    else:
+        print("YES")
+    
+def task_3():
+    arr = list(input().split(","))
+    
+    for s in arr:
+        ans = True
+        for i in s:
+            if not (i.isalpha() or i.isdigit() or i == "_"):
+                ans = False
+        if not ans:
+            print(s)
+
+import math
+def task_4():
+    data = list(input().split(" "))
+    
+    nums = []
+    for s in data:
+        if s[0].isdigit():
+            nums.append(int(s))
+        else:
+            if s == "*":
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(a * b)
+            elif s == "-":
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(b - a)
+            elif s == "+":
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(a + b)
+            elif s == "/":
+                a = nums.pop()
+                b = nums.pop()
+                nums.append(b // a)
+            elif s == "~":
+                a = nums.pop()
+                nums.append(-a)
+            elif s == "!":
+                a = nums.pop()
+                nums.append(math.factorial(a))
+            elif s == "#":
+                a = nums.pop()
+                nums.append(a)
+                nums.append(a)
+            elif s == "@":
+                a = nums.pop()
+                b = nums.pop()
+                c = nums.pop()
+                nums.append(b)
+                nums.append(a)
+                nums.append(c)
+    print(nums.pop())
+
+
+#task_1()
+#task_2()
+#task_3()
+task_4()
