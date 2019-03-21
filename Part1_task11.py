84f930500d3f997afeca5da1fc1511a4acd62fe1
Alex <alex_jel@list.ru>
2019-03-21 13:55:14 +0400

    Finished datetime task (Part1_task11).

---
+++ Part1_task11.py
@@ -0,0 +1,22 @@
+from datetime import date, timedelta
+
+
+def task_1():
+    d, m, y = map(int, input().split())
+    birth_date = date(y, m, d)
+    age = (date.today() - birth_date) // timedelta(days=365.2425)
+    print(age)
+    print(date(y + age + 1, m, d).weekday())
+
+
+def task_2():
+    d, m, y = map(int, input().split())
+    life_days = int(input())
+    diff = (date.today() - date(y, m, d)).days
+    if diff > life_days:
+        print("No")
+    else:
+        print("Yes", life_days - diff)
+
+# task_1()
+task_2()
