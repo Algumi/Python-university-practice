d138b6af91e85189dd6944267d99a192d4621016
Alex <alex_jel@list.ru>
2019-05-17 01:21:23 +0400

    Finished Part2_task3 (test application with GUI).

--- Part_2/Part_2_task3.py
+++ Part_2/Part_2_task3.py
@@ -11,12 +11,14 @@ class Application:
 
     questions = []
     current_question = 0
+    ans_var = IntVar()
 
     def start_app(self):
         self.window.title('Test')
-        self.window.geometry('600x350')
+        self.window.geometry('600x450')
         self.current_question = 0
         self.generate_questions()
+        self.destroy_located()
         self.main_page()
 
     def main_page(self):
@@ -28,25 +30,44 @@ class Application:
         self.window.mainloop()
 
     def locate_question_type1(self, q_data):
-        print("\ni m working")
         self.question_lbl.pack(side=TOP)
         self.question_lbl.configure(text=q_data[2])
         self.answer_input = [Entry(self.window, width=50)]
         self.answer_input[0].pack(side=TOP)
 
     def locate_question_type2(self, q_data):
+        self.ans_var = IntVar()
         self.question_lbl.pack(side=TOP)
         self.question_lbl.configure(text=q_data[2])
-        radio_var = IntVar()
         for i, ans in enumerate(q_data[3]):
-            radio_btn = Radiobutton(self.window, text=ans, variable=radio_var, value=i)
+            radio_btn = Radiobutton(self.window, text=ans, variable=self.ans_var, value=i)
             radio_btn.pack(side=TOP)
             self.answer_input.append(radio_btn)
 
     def locate_question_type3(self, q_data):
+        self.ans_var = [IntVar() for _ in range(len(q_data[3]))]
         self.question_lbl.pack(side=TOP)
         self.question_lbl.configure(text=q_data[2])
-        self.window.update()
+        for i, ans in enumerate(q_data[3]):
+            radio_btn = Checkbutton(self.window, text=ans, variable=self.ans_var[i], onvalue=1, offvalue=0)
+            radio_btn.pack(side=TOP)
+            self.answer_input.append(radio_btn)
+        print([x.get() for x in self.ans_var])
+        print([str(i) for i, x in enumerate(self.ans_var) if x.get() == 1])
+
+    def final_page(self):
+        wrong = [x for x in self.questions if x[1] == 2]
+        q_num, right_num = len(self.questions), len(self.questions) - len(wrong)
+        if right_num < q_num / 2:
+            self.refresh_btn = Button(self.window, text='restart', width=10, height=1, command=self.refresh_click)
+            self.refresh_btn.pack(side=BOTTOM, pady=10)
+        self.question_lbl.pack(side=TOP)
+        self.question_lbl.configure(text="Результат: %d правильных ответов из %d\nНеправильные ответы даны на вопросы:"
+                                         % (right_num, q_num))
+        for i, ans in enumerate(wrong):
+            wrong_q = Label(self.window, text=ans[2])
+            wrong_q.pack(side=TOP)
+            self.answer_input.append(wrong_q)
 
     def next_question(self, num=None):
         print(num, self.current_question, self.questions)
@@ -54,14 +75,27 @@ class Application:
             q_id = self.current_question
             cur_q = self.questions[q_id]
             if cur_q[0] == 1:
-                # print(cur_q[3] + "---" + str(self.answer_input[0].text) + "---")
-                if self.answer_input[0] == cur_q[3]:
+                if self.answer_input[0].get() == cur_q[3]:
+                    self.questions[q_id][1] = 1
+                else:
+                    self.questions[q_id][1] = 2
+            elif cur_q[0] == 2:
+                if self.ans_var.get() == int(cur_q[4]):
+                    self.questions[q_id][1] = 1
+                else:
+                    self.questions[q_id][1] = 2
+            elif cur_q[0] == 3:
+                ans_lst = [str(i) for i, x in enumerate(self.ans_var) if x.get() == 1]
+                if ans_lst == cur_q[4]:
                     self.questions[q_id][1] = 1
                 else:
                     self.questions[q_id][1] = 2
             self.current_question += 1
-        if self.current_question < len(self.questions):
-            self.destroy_located()
+
+        self.destroy_located()
+        if self.current_question == len(self.questions):
+            self.final_page()
+        else:
             self.locate_buttons()
             next_q = self.questions[self.current_question]
             if next_q[0] == 1:
@@ -72,19 +106,16 @@ class Application:
                 self.locate_question_type3(next_q)
 
     def locate_buttons(self):
-        self.refresh_btn = Button(self.window, text='restart', width=10, height=1, command=self.refresh_click)
-        self.refresh_btn.pack(side=BOTTOM, pady=10)
         self.next_btn = Button(self.window, text='next', width=10, height=1, command=self.next_question)
         self.next_btn.pack(side=BOTTOM, pady=10)
 
     def destroy_located(self):
-        if len(self.answer_input) > 0:
-            for w in self.answer_input:
-                w.destroy()
-            self.answer_input = []
-            self.question_lbl.pack_forget()
-            self.next_btn.destroy()
-            self.refresh_btn.destroy()
+        for w in self.answer_input:
+            w.destroy()
+        self.answer_input = []
+        self.question_lbl.pack_forget()
+        self.next_btn.destroy()
+        self.refresh_btn.destroy()
 
     def refresh_click(self):
         self.destroy_located()
@@ -102,7 +133,7 @@ class Application:
             elif raw_data[0] == '2':
                 self.questions.append([2, 0, raw_data[1], raw_data[2].split(" | "), raw_data[3]])
             elif raw_data[0] == '3':
-                self.questions.append([2, 0, raw_data[1], raw_data[2].split(" | "), raw_data[3].split(" | ")])
+                self.questions.append([3, 0, raw_data[1], raw_data[2].split(" | "), raw_data[3].split(" | ")])
         shuffle(self.questions)
 
 
